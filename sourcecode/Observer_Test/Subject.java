11ce5903e07e3cb35395a800405d71829670d7d5
koushik41 <Koushik.r.bright@gmail.com>
2016-04-15 23:52:22 -0700

    ObserverPattern-KTK

---
+++ sourcecode/Observer_Test/Subject.java
@@ -0,0 +1,13 @@
+/**
+ * Write a description of class Subject here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public interface Subject  
+{
+   void notifyObserver();
+   void addObserver();
+   void removeObserver();
+   
+}
