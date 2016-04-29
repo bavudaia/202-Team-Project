95206d0a16637120cec87f0224c1cf418d1a5243
Venkatesh Ramanujam <venki1092@gmail.com>
2016-04-28 21:58:57 -0700

    included mongo DB connection to fetch and store the user data

--- sourcecode/LoginScreen_Test/SignIn.java
+++ sourcecode/LoginScreen_Test/SignIn.java
@@ -1,4 +1,11 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import org.json.JSONObject;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+import com.mongodb.client.FindIterable;
+import com.mongodb.Block;
 
 /**
  * Write a description of class SignIn here.
@@ -11,12 +18,18 @@ public class SignIn extends Button
     Login myLogin;
     TextBox username;
     TextBox password;
+    MongoClient mongoClient;
+    MongoDatabase db;
+    String userPassword = "";
     /**
      * Act - do whatever the SignIn wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public SignIn()
     {
+        mongoClient = new MongoClient();
+        db = mongoClient.getDatabase("LoginDetails");
+
         myLogin = new AuthenticationProxy();
     }
     public SignIn(TextBox userName, TextBox password)
@@ -36,8 +49,27 @@ public class SignIn extends Button
             //TextBox password =(TextBox) getOneObjectAtOffset(400,240,TextBox.class);
             String passWordText = password.getText();
             System.out.println(passWordText);
-                
-            if(myLogin.authenticateLogin(usernameText,passWordText/*"TeamCMPE20218", "TestString"*/))
+            
+            FindIterable<Document> iterable = db.getCollection("userdata").find(new Document("Username",usernameText));
+            Boolean isCorrectPassword = false;
+            
+            
+            iterable.forEach(new Block<Document>() {
+            @Override
+            public void apply(final Document document) {
+                try{
+                    String documentContent = document.toJson();
+                    JSONObject jsonDoc = new JSONObject(documentContent);
+                    userPassword =  jsonDoc.getString("Password");
+                    
+                }
+                catch(Exception ex)             
+                {
+                    System.out.println(ex.toString());
+                }
+            }
+        });
+            if(myLogin.authenticateLogin(usernameText,userPassword/*"TeamCMPE20218", "TestString"*/))
             {
                 BlackJackWorld blackJackWorld = new BlackJackWorld();
                 Greenfoot.setWorld(blackJackWorld);
